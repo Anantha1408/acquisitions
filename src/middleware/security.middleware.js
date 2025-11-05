b1e767629d696b4413ec7cd16aa9e5d7c70997d8
Anantha <krisnaanantha149@gmail.com>
2025-11-05 20:31:55 +0530

    implemented security middleware

---
+++ src/middleware/security.middleware.js
@@ -0,0 +1,66 @@
+import aj from '#config/arcjet.js'
+import res from "express/lib/response.js";
+import req from "express/lib/request.js";
+import {slidingWindow} from "@arcjet/node";
+import logger from "#config/logger.js";
+import app from "../app.js";
+
+const securityMiddleware=async (req,res,next)=> {
+    try {
+        const role = req.user?.role || 'guest';
+        let limit;
+        let message;
+        switch (role) {
+            case 'admin':
+                limit = 20
+                message = 'admin request limit exceeded'
+                break;
+            case 'user':
+                limit = 10
+                message = 'user request limit exceeded'
+                break;
+            case 'guest':
+                limit = 5
+                message = 'guest request limit exceeded'
+                break;
+        }
+        const client = aj.withRule(slidingWindow({
+            mode: 'LIVE',
+            interval: '1m',
+            max: limit,
+            name: `${role}-rate-limit`
+        }));
+
+        const decision = await client.protect(req);
+        if (decision.isDenied() && decision.reason.isBot()) {
+            logger.warn('Bot request blocked', {ip: req.ip, userAgent: req.get('User-Agent'), path: req.path});
+            return res.status(403).json({error: 'Forbidden', message: 'Automated requests are not allowed'});
+
+        }
+        if (decision.isDenied() && decision.reason.isShield()) {
+            logger.warn('Shield request blocked', {
+                ip: req.ip,
+                userAgent: req.get('User-Agent'),
+                path: req.path,
+                method: req.method
+            });
+            return res.status(403).json({error: 'Forbidden', message: 'Request blocked by security policy'});
+
+        }
+
+        if (decision.isDenied() && decision.reason.isRateLimit()) {
+            logger.warn('Rate limit exceeded', {ip: req.ip, userAgent: req.get('User-Agent'), path: req.path});
+            return res.status(403).json({error: 'Forbidden', message: 'too many requests are not allowed'});
+
+        }
+        next();
+    } catch (e) {
+        console.error('arcjet middleware error', e);
+        res.status(500).json({
+            error: 'Internal Server Error',
+            message: 'Something went wrong with security middleware'
+        });
+
+    }
+}
+export default securityMiddleware;
